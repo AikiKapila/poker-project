1f837a0754840f22ad3e196db82a786496a24dc6
AikiKapila <aikikapila@seattleacademy.org>
2024-12-09 12:46:55 -0800

    Update pokergame.py

--- pokergame.py
+++ pokergame.py
@@ -1,3 +1,17 @@
 import pygame
 import time
 
+######## CARD CREATION ########
+
+class Card:
+    def __init__(self, number, suit):
+        self.n = number
+        match suit:
+            case 0:
+                self.s = "clubs"
+            case 1:
+                self.s = "diamonds"
+            case 2:
+                self.s = "hearts"
+            case 3:
+                self.s = "spades"

78661043df51cad12200adac9beff9f3ed5c64b5
AikiKapila <aikikapila@seattleacademy.org>
2024-12-17 11:54:09 -0800

    Co-authored-by: aohuu <aohuu@users.noreply.github.com>

--- pokergame.py
+++ pokergame.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-import time
 
 
 # Pygame Set Up #
@@ -9,7 +8,6 @@ screen_width = 1400
 screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Poker Game')
-
 
 # CARD CREATION #
 class Card:
@@ -31,7 +29,6 @@ class Card:
     def set_image(self, image):
         self.image = image
         
-        
 # Attaching Card Images to Names #
 def load_card_images(cards):
     for card in cards:
@@ -47,43 +44,66 @@ def create_deck():
     deck = [Card(number, suit) for suit in suits for number in range(1, 13)]
     return deck
 
-
-# Creating the deck and images #
-deck = create_deck()
-load_card_images(deck)
-random.shuffle(deck)
-
 # Function to draw a card from the deck # //You need to add somewhere for the popped card to land (player/ai hand)
 def draw_card(deck, hand):
-    if deck:
+    if deck:   
         hand.append(deck[0])
         deck.pop(0)
     else:
         return None  # Return None if the deck is empty
 
-def display_card(card):
-    pass
+def draw_hand(draw_num, deck, hand):
+    for i in range(draw_num):
+        draw_card(deck, hand)
+
+def display_card(card, index, total_cards):
+    if card.image:
+        # Get original dimensions of image #
+        original_width, original_height = card.image.get_size()
+    
+        # Calculate new dimensions #
+        card_width = original_width * 0.15
+        card_height = original_height * 0.15
+
+        scaled_image = pygame.transform.scale(card.image, (card_width, card_height))
+
+        spacing = 20
+        total_width = total_cards * card_width + (total_cards - 1) * spacing
+        start_x = (screen_width - total_width) // 2
+        x_position = start_x + index * (card_width + spacing)
+        y_position = screen_height * 5 // 6 - card_height
+
+        # Debugging #
+        print(f"Card {card.n}{card.s} at position ({x_position}, {y_position})")
+
+        screen.blit(scaled_image, (x_position, y_position))
+
+def display_hand(hand):
+    for index, card in enumerate(hand):
+        display_card(card, index, len(hand))
+
+
+# Creating the deck and images and empty player hand #
+deck = create_deck()
+load_card_images(deck)
+random.shuffle(deck)
+player_hand = []
+
 
 # Main Game Loop #
 running = True
+card_drawn = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     screen.fill((0, 128, 0))
+    if not card_drawn:
+       draw_hand(2, deck, player_hand)
+       card_drawn = True
 
-    if deck[0].image:
-        # Get original dimensions of image #
-        original_width, original_height = deck[0].image.get_size()
-        
-        # Calculate new dimensions #
-        new_width = original_width * 0.15
-        new_height = original_height * 0.15
-
-        scaled_image = pygame.transform.scale(deck[0].image, (new_width, new_height))
-        screen.blit(scaled_image, (screen_width // 2 - new_width // 2, screen_height // 2 - new_height // 2))
-
+    
     pygame.display.flip()
 
 pygame.quit()
